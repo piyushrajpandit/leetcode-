52d9ce4e3ccc9fadbd0c8e2163ecbe869ac458ca
piyushrajpandit <piyush06raj04@gmail.com>
2026-03-14 14:59:07 +0530

    Add reverseString function using stack
    Implement a function to reverse a string using a stack.

---
+++ 344ReverseString.cpp
@@ -0,0 +1,19 @@
+// #apprach 
+// 1. stacl me dalo aur nikal lo 
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+     stack<char> st ;
+     for(int i = 0 ; i < s.size() ; i++ ){
+             st.push(s[i]);
+     }
+  for(int i = 0; i < s.size(); i++){
+            s[i] = st.top();
+            st.pop();
+    
+
+        
+    }
+    }
+};

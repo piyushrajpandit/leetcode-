8ec7d9baae28f33b287b9ec05b7a01b8db44d0a2
piyushrajpandit <piyush06raj04@gmail.com>
2026-03-23 19:54:57 +0530

    Add function to count leaf nodes in binary tree
    Implement function to count leaf nodes in a binary tree.

---
+++ countNoofleafNode.cpp
@@ -0,0 +1,16 @@
+void inorder(BinaryTreeNode<int> * root, int & count){
+//base case
+if(root ==NULL){
+return ;
+}
+inorder(root->left,count);
+if(root->left == NULL && root->right == NULL){
+count++;
+}
+inorder(root->right,count);
+}
+int noOfLeafNodes(BinaryTreeNode<int> * root){
+int cnt =0 ;
+inorder(root,cnt);
+return cnt;
+}

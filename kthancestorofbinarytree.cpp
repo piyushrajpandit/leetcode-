15523128d93d44cefc60cd518bd88aa8331e5088
piyushrajpandit <piyush06raj04@gmail.com>
2026-03-28 00:09:46 +0530

    Enhance comments for kth ancestor logic
    Add comments to clarify the logic for finding the kth ancestor in a binary tree.

--- kthancestorofbinarytree.cpp
+++ kthancestorofbinarytree.cpp
@@ -1,3 +1,12 @@
+// actually what are we doning here we are finding ancestor here 
+// so root data match with node we have we will return root 
+// if not find left ans and same for right aswer 
+// and we if leftans we get node and right null we will do k -- and if at any place k becomes 0 or less than 0 we will make k = int max so k never became 0 again and return the root 
+
+// and if ans = nULL or ans data == node then means(already top node hai) no ancestor return null
+// else return ans->data
+
+
 Node* solve(Node* root, int &k , int node){
 //base case 
 if(root == NULL)

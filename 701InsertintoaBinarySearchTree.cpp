8a4f6e82e93b32c3e8c32fa1b1081be8f70f0c81
piyushrajpandit <piyush06raj04@gmail.com>
2026-03-29 15:05:03 +0530

    Implement insertIntoBST function for binary search tree

---
+++ 701InsertintoaBinarySearchTree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == NULL){
+            root = new TreeNode(val);
+            return root;
+
+        }
+        if(val > root->val){
+            //insert at right part
+            root->right = insertIntoBST(root->right, val);
+        }
+        else if (val < root->val){
+            //insert into left part 
+            root->left = insertIntoBST(root->left, val);
+        }
+        return root;
+    }
+};

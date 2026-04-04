f445a8ccf121fdfbdd7848223ba9966411be77c5
piyushrajpandit <piyush06raj04@gmail.com>
2026-04-04 23:52:06 +0530

    Create insertinheap.cpp

---
+++ insertinheap.cpp
@@ -0,0 +1,24 @@
+// time o(longn)
+#include<iostream>
+using namespace std;
+
+class heap {
+public:
+int arr[100];
+int size =0 ;
+void insert(int val){
+  size = size+1;
+int index = size;
+arr[index] = val;
+while(index> 1){
+int parent = index/2;
+if(arr[parent] < arr[index]){
+swap(arr[parent], arr[index]);
+index = parent;
+}
+else{
+return ;
+}
+}
+}
+};

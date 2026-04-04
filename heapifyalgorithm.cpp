fc9979c4505012786cc0e142411e684bdc29eb82
piyushrajpandit <piyush06raj04@gmail.com>
2026-04-05 00:37:11 +0530

    Add heapify function to maintain heap property
    Implement heapify function for heap data structure.

---
+++ heapifyalgorithm.cpp
@@ -0,0 +1,15 @@
+void heapify(int arr[], int , int i){
+  int largest =i;
+int left = 2*i;
+int right = 2*i+1;
+if(left < n && arr[largest] < arr[left]){
+largest = left;
+}
+if(right < n && arr[largest] < arr[right]){
+largest = right;
+}
+if(largest != i){
+swap(arr[largest], aarr[i]);
+heapify(arr, n, largest);
+}
+}

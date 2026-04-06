4699f0319bae847a5cf693d43c5605b7b4d54703
piyushrajpandit <piyush06raj04@gmail.com>
2026-04-06 07:40:14 +0530

    Add function to get k-th largest sum of subarrays
    Implement function to find the k-th largest sum of subarrays.

---
+++ kthlargestsumsubarray.cpp
@@ -0,0 +1,21 @@
+int getkthlargest(vector<int> & arr, int k ) {
+  priority_queue<int, vector<int> , greaater<int>> mini;
+
+int n= arr.size();
+for(int i =0 ; i< n; i++){
+int sum =0;
+for(int j=0; j< n;j++){
+sum+= arr[j];
+if(mini.size() < k){
+mini.push(sum);
+}
+else{
+if(sum > mini.top()){
+mini.pop();
+mini.push(sum);
+}
+}
+}
+}
+return mini.top();
+}

07d545c35783fb20c8f5695850acc5f493db944d
piyushrajpandit <piyush06raj04@gmail.com>
2026-04-07 16:59:54 +0530

    Create getKthLargest.cpp

---
+++ getKthLargest.cpp
@@ -0,0 +1,13 @@
+int getKthLargest(vector<int> &arr, int k){
+vector<int> sumStore;
+int n = arr.size();
+for(int i=0 ; i< n; i++){
+int sum = 0;
+for(int j = i; j < n; j++){
+sum += arr[j];
+sumStore.push_back(sum);
+}
+}
+sort(sumStore.begin(), sumStore.end());
+return sumStore[sumStore.size()-k];
+}

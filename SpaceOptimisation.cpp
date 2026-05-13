ae75285a3580857d593898f7a0a5530dee1d6a91
Piyush <piyush06raj04@gmail.com>
2026-05-13 19:12:23 +0530

    Add space-optimized Fibonacci implementation
    Implement Fibonacci calculation using space optimization.

---
+++ SpaceOptimisation.cpp
@@ -0,0 +1,14 @@
+int main(){
+int n;
+ cin >> n; 
+int prev1 =1;
+int prev2 = 0;
+for(int i =2 ; i<= n; i++){
+int curr = prev1 +prev2;
+//shift logic 
+prev2 = prev1;
+prev1 = curr;
+}
+cout << prev1 << endl;
+return 0;
+}

fc6fc40876de78c00c9ab04286537211a90141df
Piyush <piyush06raj04@gmail.com>
2026-07-09 17:34:01 +0530

    Add solution for erasing overlapping intervals
    Implement solution for 435 Non-overlapping Intervals problem.

---
+++ 435NonoverlappingIntervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int> &b){
+        return a[1] < b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.empty())
+            return 0;
+        sort(intervals.begin(), intervals.end() ,cmp);
+        int count =1 ;
+        int n = intervals.size();
+        int ansEnd = intervals[0][1];
+        for(int i =1 ;i<n; i++){
+            if(intervals[i][0] >= ansEnd){
+                count++;
+                ansEnd = intervals[i][1];
+            }
+        }
+        return n- count;
+    }
+};

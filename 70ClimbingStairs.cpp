06a79e3621013d411ec8bd8cbe37c8c2a67efdc5
Piyush <piyush06raj04@gmail.com>
2026-07-11 05:17:55 +0530

    Implement climbing stairs solution in C++

---
+++ 70ClimbingStairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+            return n;
+        int first =1, second =2 ,current;
+        for(int i =3 ; i<=n ; i++){
+            current = first + second;
+            first = second;
+            second = current;
+        }
+        return second;
+    }
+};

88075a7e39362ebd45e1630be07594d0d9b4fd7e
Piyush <piyush06raj04@gmail.com>
2026-08-31 15:52:00 +0530

    Implement binary search for arranging coins

---
+++ 441ArrangingCoins.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int left =0 ;
+        int right = n;
+       
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            long long temp = 1LL* mid*(mid + 1 ) /2;
+            if(temp == n){
+                return mid;
+            }
+            if(temp < n){
+                left = mid+ 1;
+             }
+            else{
+                right = mid -1;
+            }
+        }
+    return right;
+    }
+};

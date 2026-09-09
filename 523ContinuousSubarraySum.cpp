8c7cbd6d5649bf108c733ed1ec257463793a2470
Piyush <piyush06raj04@gmail.com>
2026-09-09 17:03:25 +0530

    Implement checkSubarraySum function in Solution class

---
+++ 523ContinuousSubarraySum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int prefixSum = 0;
+        for(int i =0 ; i< nums.size() ; i++){
+            prefixSum += nums[i];
+            int remainder = prefixSum % k;
+
+            if(mp.find(remainder) == mp.end()){
+                mp[remainder] =i;
+            }
+            else{
+                if(i - mp[remainder] >= 2)
+                    return true;
+            }
+        }
+ 
+        return false;
+    }
+};

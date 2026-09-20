70bee45cd0653fc06a0756e4fe3b611a3e4f241c
Piyush <piyush06raj04@gmail.com>
2026-09-20 06:13:05 +0530

    Implement rearrangeArray function to separate signs

---
+++ 2149RearrangeArrayElementsbySign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int j =0;
+        int k = 1;
+        for(int i =0 ; i< nums.size() ;i++){
+            if(nums[i] > 0){
+                ans[j] = nums[i];
+                j = j+2;
+            }
+            else{
+                ans[k] = nums[i];
+                k = k+2;
+            }
+        }
+        return ans;
+    }
+};

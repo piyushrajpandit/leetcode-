516616ac2de4723bd66c6c48b1bc3e5d25b90de4
Piyush <piyush06raj04@gmail.com>
2026-09-24 06:39:44 +0530

    Implement smallestIndex function to find index

---
+++ 3550SmallestIndexWithDigitSumEqualtoIndex.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        for(int i =0 ; i< nums.size(); i++){
+            int sum = 0;
+            int temp = nums[i];
+            while(temp > 0){
+            int digit =  temp % 10 ;
+            sum += digit;
+            temp = (temp - digit)/10;
+            }
+            if(sum == i){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+};
